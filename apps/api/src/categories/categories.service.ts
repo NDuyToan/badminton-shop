5b5fe792cb8f5f3e26222c216ca949c374b055ed
toan_nguyen <nguyenduytoan@bkdn@gmail.com>
2026-08-24 10:25:27 +0700

    feat: crud category

--- apps/api/src/categories/categories.service.ts
+++ apps/api/src/categories/categories.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+
 
 @Injectable()
 export class CategoriesService {
   constructor(private readonly prisma: PrismaService) { }
 
-  create(createCategoryDto: CreateCategoryDto) {
-    return this.prisma.category.create({
-      data: createCategoryDto,
-    })
+  async create(createCategoryDto: CreateCategoryDto) {
+    try {
+      const category = await this.prisma.category.create({
+        data: createCategoryDto,
+      })
+      return category
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException(`Category slug '${createCategoryDto.slug}' already exists`)
+        }
+      }
+      throw error
+    }
   }
+
   findAll() {
     return this.prisma.category.findMany({
       orderBy: {
@@ -19,22 +32,46 @@ export class CategoriesService {
       }
     })
   }
-  findOne(id: number) {
-    return this.prisma.category.findUnique({
+
+  async findOne(id: number) {
+    const category = await this.prisma.category.findUnique({
       where: {
         id,
       },
     })
+
+    if (!category) {
+      throw new NotFoundException(`Category with id ${id} not found`)
+    }
+
+    return category
   }
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return this.prisma.category.update({
-      where: {
-        id,
-      },
-      data: updateCategoryDto
-    })
+
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    await this.findOne(id)
+
+    try {
+      return await this.prisma.category.update({
+        where: {
+          id,
+        },
+        data: updateCategoryDto
+      })
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException(`Category slug '${updateCategoryDto.slug}' already exists`)
+        }
+      }
+      throw error
+    }
+
+
   }
-  remove(id: number) {
+
+  async remove(id: number) {
+    await this.findOne(id)
+
     return this.prisma.category.delete({
       where: {
         id
